11a4e14afbee9551dc1d458d93421e7b8aec3945
RusgyCufu <rusgy@yandex.ru>
2021-09-24 14:37:55 +0300

    initaial commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+int main() {
+	Complex<double> cx1(1, 1), cx2(1, 1);
+	std::cout << abs(cx1);
+}
---
+++ stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+template <typename T>
+class Complex;
+
+#include <iostream>
+#include <ios>
+#include <iomanip>
+#include <vector>
+#include <set>
+
+#include "Complex.h"
+
